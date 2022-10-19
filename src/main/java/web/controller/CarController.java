2512f52a358f6c7ce88457565c9bd699bcbb41b1
andrew <vip.paevskiy@gmail.com>
2022-10-20 01:44:17 +0300

    change html to show a table

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
 import web.services.CarsService;
@@ -18,10 +19,11 @@ public class CarController {
 	private CarsService carsService;
 
 	@GetMapping(value = "/cars")
-	public String printCars(ModelMap model, @RequestParam("count") int count) {
+	public String printCars(ModelMap model,
+							@RequestParam() int count) {
 		List<Car> cars = carsService.getCars(count);
 		model.addAttribute("cars", cars);
 		return "cars";
 	}
-	
+
 }
